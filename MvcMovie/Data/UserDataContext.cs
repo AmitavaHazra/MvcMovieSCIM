70436cb42c563aa779b17294b9b1e0fb102cbd4c
Owen Patey <owen.patey@tracsis.com>
2020-05-21 16:39:18 +0100

    Add User Model and user data context with minimal user properties

---
+++ MvcMovie/Data/UserDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class UserDataContext : DbContext
+    {
+        public UserDataContext(DbContextOptions<UserDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MvcMovie/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace MvcMovie.Models
+{
+    /// <summary>
+    /// Internal application user representation
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Primary key for user
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// SCIM external Identifier
+        /// </summary>
+        public string ExternalId { get; set; }
+
+        /// <summary>
+        /// User's display name
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Azure AD sends user's UPN here
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Whether the Azure AD user is Active
+        /// </summary>
+        public bool Active { get; set; }
+    }
+}
